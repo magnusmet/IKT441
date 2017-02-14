aa6fe2a803e3df6909221eaf579ca99eaaa99107
Jone Bakkevig <jonekb13@student.uia.no>
2017-02-14 17:04:39 +0100

    Updated svm

--- SVM/svm.py
+++ SVM/svm.py
@@ -7,26 +7,11 @@ import warnings
 warnings.simplefilter("ignore")
 
 data = [[float(i) for i in i.split()] for i in open("yeast.data").readlines() if i.strip()]
-training = [[float(i) for i in i.split()] for i in open("yeast_tr.data").readlines() if i.strip()]
-testing = [[float(i) for i in i.split()] for i in open("yeast_tst.data").readlines() if i.strip()]
-
-y_training = []
-y_testing = []
-y_data = []
-
-for i in training:
-        y_training.append(i[-1])
-        del i[-1]
-
-for i in testing:
-        y_testing.append(i[-1])
-        del i[-1]
 
 train, test = train_test_split(data,test_size=0.1)
 
 y_train = []
 y_test = []
-
 
 for i in train:
         y_train.append(i[-1])
@@ -36,51 +21,48 @@ for i in test:
         y_test.append(i[-1])
         del i[-1]
 
-
-
-
-
-X = np.array(training)
-Y = np.array(y_training)
-X_ = np.array(testing)
-Y_ = np.array(y_testing)
-X2 = np.array(train)
-Y2 = np.array(y_train)
-X_2 = np.array(test)
-Y_2 = np.array(y_test)
-
+X = np.array(train)
+Y = np.array(y_train)
+X_ = np.array(test)
+Y_ = np.array(y_test)
 
 C=1.0
 gamma = 0.5
-svm_linear = svm.SVC(kernel='linear',C=C,gamma=gamma).fit(X2,Y2)
-svm_polynomial = svm.SVC(kernel='poly',C=C,gamma=gamma).fit(X2,Y2)
-svm_rbf = svm.SVC(kernel='rbf',C=C,gamma=gamma).fit(X2,Y2)
-svm_sigmoid = svm.SVC(kernel='sigmoid',C=C,gamma=gamma).fit(X2,Y2)
+svm_linear = svm.SVC(kernel='linear',C=C,gamma=gamma).fit(X, Y)
+svm_polynomial = svm.SVC(kernel='poly',C=C,gamma=gamma).fit(X, Y)
+svm_rbf = svm.SVC(kernel='rbf',C=C,gamma=gamma).fit(X, Y)
+svm_sigmoid = svm.SVC(kernel='sigmoid',C=C,gamma=gamma).fit(X, Y)
 
-for i in range(100):
+avgLin = 0.0
+avgPol = 0.0
+avgRbf = 0.0
+avgSig = 0.0
+
+run = 1000
+
+for i in range(run):
     print("Testing round: "),i
     print("Linear")
-    print(svm_linear.score(X_2,Y_2))
+    print(svm_linear.score(X_, Y_))
+    avgLin += svm_linear.score(X_, Y_)
     print("Poly")
-    print(svm_polynomial.score(X_2,Y_2))
+    print(svm_polynomial.score(X_, Y_))
+    avgPol += svm_polynomial.score(X_, Y_)
     print("RBF")
-    print(svm_rbf.score(X_2,Y_2))
+    print(svm_rbf.score(X_, Y_))
+    avgRbf += svm_rbf.score(X_, Y_)
     print("Sigmoid")
-    print(svm_sigmoid.score(X_2,Y_2))
+    print(svm_sigmoid.score(X_, Y_))
+    avgSig += svm_sigmoid.score(X_, Y_)
+
+print("Average Linear Performance:", (avgLin/run))
+print("Average Poly Performance:", (avgPol/run))
+print("Average RBF Performance:", (avgRbf/run))
+print("Average Sigmoid Performance: ", (avgSig/run))
 
 
-# def testSVM(svm):
-#     numcorrect = 0
-#     numwrong = 0
-#     for correct,testing in ((0,zero),(1,one)):
-#         for d in testing:
-#             r = svm.predict(d)[0]
-#             if(r==correct):
-#                 numcorrect += 1
-#             else:
-#                 numwrong += 1
-#     print "Correct",numcorrect
-#     print "Wrong",numwrong
+
+
 
 
 
